c26767dfeb81e5562053952b309fdaa5cb0d5003
Marek Prihoda <marek.prihoda@gmail.com>
2011-11-11 19:47:37 +0100

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.1'
+gem 'sqlite3'
+gem "bcrypt-ruby", :require => "bcrypt"
+
+group :assets do
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development do
+  gem 'awesome_print'
+  gem 'hirb'
+  gem 'hash_syntax'
+  gem 'annotate'
+end
+
+group :test, :development do
+  gem 'rspec-rails'
+  gem 'capybara'
+  gem 'factory_girl_rails'
+  gem 'launchy'
+  gem 'spork'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrimalAuth::Application.config.secret_token = '4eecef4de82d0b3fa52639c81200ed6c3df57c28bd4b3e47c2221daf3271a820ed1f13438f26a60b74021e87197eb7b83166b143ca94bdc8b1f41007175f4b00'
